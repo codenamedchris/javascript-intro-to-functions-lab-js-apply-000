69a7b5ab75bacb899e03819769775516e16826d6
Christina Poole <codenamedchris@gmail.com>
2020-08-04 19:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  for string.toLowerCase()
+  for (string.toLowerCase()
   return "I can't hear you!"
   (string.toUpperCase())
   return "YES INDEED!"
