a3ca6e3400b7bbdabc54c5c8a03f22452987f44e
Christina Poole <codenamedchris@gmail.com>
2020-08-04 18:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase('HELLO')
 }
+
+function logShout(string){
+  console.log(string.toUpperCase('hello'))
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase('HELLO'))
+}
